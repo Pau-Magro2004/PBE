a8be2a0906094a960146e771f8bed288a18af926
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-12 18:19:32 +0200

    Update cliente.js

--- CLIENTE-SERVIDOR-WEB/cliente.js
+++ CLIENTE-SERVIDOR-WEB/cliente.js
@@ -10,20 +10,20 @@ function handleQueryClick() {
   performRequest(url,updateSentQuery);
 }
 
-function updateLoggedUser(data){  
-     username = data.name;
-     if (username === null){ //Si no coincide con nada en la base de datos
+function updateLoggedUser(response){  
+     
+     if (response.status === 'id_not_matched'){ //Si no coincide con nada en la base de datos
         const error_login = document.getElementById('login-message')
         error_login.textContent = 'Usuario o contraseña incorrectos';
         error_login.classList.remove('hidden');   
         error_login.classList.add('error'); //Para el css            
      }
-     else{
-        document.getElementById('login-section').classList.add('hidden');        //Escondemos la página de log in
-        const query_page = document.getElementById('query-section');             //Obtenemos una referencia a la pagina de querys
-        query_page.classList.remove('hidden');                                   //Hacemos visible la página de querys
+     else{ //'id_matched'
+        document.getElementById('login-section').classList.add('hidden');                //Escondemos la página de log in
+        const query_page = document.getElementById('query-section');                     //Obtenemos una referencia a la pagina de querys
+        query_page.classList.remove('hidden');                                           //Hacemos visible la página de querys
         const label = document.getElementById('welcome-label');
-        label.textContent= `Welcome ${username}`!;                              //Ponemos el texto en el label del HTML  
+        label.textContent= `Welcome ${response.data.name}`!;                              //Ponemos el texto en el label del HTML  
      }
 }
 function updateSentQuery(data){
