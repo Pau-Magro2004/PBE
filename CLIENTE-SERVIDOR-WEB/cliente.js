1efa1db0ba51fe23aeb0c5846d6b8c0f58c25589
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-12 11:27:54 +0200

    Update cliente.js

--- CLIENTE-SERVIDOR-WEB/cliente.js
+++ CLIENTE-SERVIDOR-WEB/cliente.js
@@ -8,72 +8,7 @@ function handleQueryClick() {
   const url = buildQueryUrl(query);
   performRequest(url,updateSentQuery);
 }
-function performRequest(url,callback){
-   const com = new ServerCommunication(url,callback);
-   com.sendRequest();
-}
 
-// Obtiene las credenciales desde el formulario
-function getCredentials() {
-  const id   = document.getElementById('user-id').value;
-  const pass = document.getElementById('user-pass').value;
-  return { id, pass };
-}
-
-// Construye la URL de login
-function buildLoginUrl({ id, pass }) {
-  const base   = 'http://localhost:8000/Servidor/login.php/students?';
-  const params = `id=${encodeURIComponent(id)}&pass=${encodeURIComponent(pass)}`;
-  return base + params;
-}
-function buildQueryUrl(query) {
-  const base = 'http://localhost:8000/Servidor/querys.php/${query}?';
-  let constraints;
-  switch (query) {
-    case "marks": constraints = ""; break;
-    case "timetables": constraints = "day=now&hour[gt]=now"; break;
-    case "tasks": constraints = "date[gte]=now"; break;
-    default: constraints = null; // Invalid table
-   }
-return base + constraints;
-}
-  
-
-class ServerCommunication {
-  constructor(url,callback){
-     this.url = url;
-     this.callback = callback;
-     this.xhr = null;    
-  }
-  sendRequest(){
-     this.xhr = new XMLHttpRequest();
-     this.xhr.open('GET', this.url, true);
-     this.xhr.onload =  () => {
-      this.handleServerResponse();
-     };
-     this.xhr.onerror = () => {
-      console.error('Error de conexión');
-     };
-     this.xhr.send();    
-  }
-  handleServerResponse(){
-    const xhr = this.xhr;
-    if(xhr.status === 200){
-      try{
-        const response = JSON.parse(xhr.responseText);       //Parseamos la respuesta .json del server
-        if (response.status === 'id_matched'){
-          this.callback(response.data);}
-        else{
-          this.callback(null);}
-      }catch(e){
-        console.error('Error al procesar JSON:', e);
-      }
-    }
-    else{
-       console.error(`Error del servidor: ${xhr.status}`);
-     }
-   }
-}
 function updateLoggedUser(username){  
      if (username === null){ //Si no coincide con nada en la base de datos
         const error_login = document.getElementById('login-message')
