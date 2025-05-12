6d081e1dc92c8828d85e004f5ba9475aaacb36a2
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-12 09:04:15 +0200

    Update logica.js

--- CLIENTE-SERVIDOR-WEB/logica.js
+++ CLIENTE-SERVIDOR-WEB/logica.js
@@ -6,26 +6,6 @@ function handleQueryClick() {
   const url =
   const com = serverCommunication(url,updateSentQuery);
 }
-//Esta función es la que ha de tener el callback, es decir, tanto para las querys como para logearte ejecutarás esta función con la diferencia que la función de callback será una u otra
-function handleServerResponse(xhr,callback){
-   if(xhr.status === 200){
-      const response = JSON.parse(xhr.responseText);  //Parseamos la respuesta .json del server
-      callback(response.data);                        //Esto te accede directamente al nombre      
-   }
-   else{
-      
-  }
-}
-
-//Envia la peticion al servidor y luego cuando detecta respuesta del servidor, ejecuta la función de callback
-function sendRequest(url,callback){
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onload = function() { //Cuando se detecte respuesta del servidor, se ejecutara la función de callback pasando el propio objeto xhr por parámetro
-     handleServerResponse(this,callback);
-  };   
-  xhr.send();
-}
 
 // Obtiene las credenciales desde el formulario
 function getCredentials() {
@@ -40,6 +20,31 @@ function buildLoginUrl({ id, pass }) {
   const params = `id=${encodeURIComponent(id)}&pass=${encodeURIComponent(pass)}`;
   return base + params;
 }
+class serverCommunication {
+  constructor(url,callback){
+    this.url = url;
+    this.callback = callback;
+    this.xhr = null;    
+  }
+  sendRequest(){
+     xhr = new XMLHttpRequest();
+     xhr.open('GET', this.url, true);
+     xhr.onload =  () => {
+      this.handleServerResponse();
+    };
+     xhr.send();    
+  }
+  handleServerResponse(){
+   if(this.xhr.status === 200){
+      const response = JSON.parse(this.xhr.responseText);  //Parseamos la respuesta .json del server
+      this.callback(response.data);                        //Esto te accede directamente al nombre      
+   }
+   else{
+      
+   }
+ }
+}
+
 document
   .getElementById('login-button')
   .addEventListener('click', handleLoginClick);
@@ -51,33 +56,11 @@ function updateLoggedUser(username){
       document.getElementById('login-section').classList.add('hidden');        //Escondemos la página de log in
       const query_page = document.getElementById('query-section');             //Obtenemos una referencia a la pagina de querys
       query_page.classList.remove('hidden');                                   //Hacemos visible la página de querys
-
       const label = document.getElementById('welcome-label');
       label.textContent= `Welcome ${username}`!;                              //Ponemos el texto en el label del HTML  
 }
 
-class serverCommunication {
-  constructor(url,callback){
-    this.url = url;
-    this.callback = callback;
-    this.xhr = new XMLHttpRequest();
-  }
-  sendRequest(){
-     xhr.open('GET', this.url, true);
-     xhr.onload = function() { //Cuando se detecte respuesta del servidor, se ejecutara la funcíon pasando el propio objeto xhr por parámetro
-     handleServerResponse(this.xhr,callback);
-     };   
-     xhr.send();    
-  }
-  handleServerResponse(xhr,callback){
-   if(xhr.status === 200){
-      const response = JSON.parse(xhr.responseText);  //Parseamos la respuesta .json del server
-      callback(response.data);                        //Esto te accede directamente al nombre      
-   }
-   else{
-      
-  }
-}
+
   
 
 
