5ef4a752b062937b50c624855bb4381a791e6ef5
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-12 17:33:59 +0200

    Update login.php

--- CLIENTE-SERVIDOR-WEB/login.php
+++ CLIENTE-SERVIDOR-WEB/login.php
@@ -4,11 +4,12 @@ ini_set('session.use_only_cookies',1); //Esto es para decirle al server que no t
 session_start(); //Si el Cliente no envia ninguna cookie (process_uid, aun no hemos iniciado sesion), esto te genera una nueva PHPSESSID que se devolverá al cliente. Si al hacer esto, tenemos una cookie existente (en querys.php) accederemos a las variablas de sesion guardadas.
 session_regenerate_id(true); //Genera un nuevo identificador de sesion (PHPSESSID) para que el anterior deje de ser válido
 
-$user_id = $_GET['id'];
+$password = $_GET['password'];
+$username = $_GET['username'];
 include("connect.php");   //Abrimos la conexión con la BD
 include("clases.php");
 
-$login = new LogIn($connexion,$user_id);
+$login = new LogIn($connexion,$password,$username);
 $_SESSION['user_id'] = $user_id;
 $_SESSION['connexion'] = true;
 $_SESSION['last_activity'] = time();
