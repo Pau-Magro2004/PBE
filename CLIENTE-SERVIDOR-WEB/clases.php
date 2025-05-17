f29219f5cb91b0bf5ec944fe96db9598e7f09b02
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-17 20:11:20 +0200

    Update clases.php

--- CLIENTE-SERVIDOR-WEB/clases.php
+++ CLIENTE-SERVIDOR-WEB/clases.php
@@ -209,7 +209,7 @@ class LogIn {
             ];                                
         }
         header('Content-Type: application/json; charset=utf-8');
-        return json_encode($response);
+        return $response;
     }
 }
 
